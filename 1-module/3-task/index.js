6ed6070a176538bd302724ac6e18c2a438a8cb9a
Vlad Bachinskiy <bochalife@gmail.com>
2024-04-29 13:36:50 +0300

    3 task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
-  // ваш код...
+  let newStr = str[0].toUpperCase() + str.slice(1);
+
+}
+
+function ucFirst(str) {
+  if (!str) return str;
+
+  return str[0].toUpperCase() + str.slice(1);
 }
